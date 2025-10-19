94000d8b150194ff2dd3dd6b866bd00686f0152f
Himel <shahriarhasan221@gmail.com>
2025-10-19 21:29:03 +0600

    added categories

--- src/component/homelayout/SocialLogin.jsx
+++ src/component/homelayout/SocialLogin.jsx
@@ -9,11 +9,11 @@ const SocialLogin = () => {
       <h2 className="font-bold mb-5">Login with</h2>
       <div className="flex flex-col space-y-3">
         <button className="btn btn-outline btn-secondary">
-          <FcGoogle />
+          <FcGoogle size={20} />
           Login with Google
         </button>
         <button className="btn btn-outline btn-primary">
-          <FaGithub />
+          <FaGithub size={20} />
           Login with GitHub
         </button>
       </div>
